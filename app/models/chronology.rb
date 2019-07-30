e5302df5197483b458c9a73ffd4923ca1375fa47
yukimura1227 <takamura1227@gmail.com>
2019-07-31 00:02:32 +0900

    chore: add denenent: :destroy option to chronology

--- app/models/chronology.rb
+++ app/models/chronology.rb
@@ -1,4 +1,4 @@
 # for Chronology
 class Chronology < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
 end
--- spec/models/chronology_spec.rb
+++ spec/models/chronology_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Chronology, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    describe 'belongs_to' do
+      it { is_expected.to belong_to(:user).dependent(:destroy) }
+    end
+  end
 end
