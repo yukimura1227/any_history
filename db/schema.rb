95bd72c572040ec76711d719bf6af6a9063826cf
yukimura1227 <takamura1227@gmail.com>
2019-07-30 23:28:29 +0900

    chore: adjust schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_09_26_142813) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.integer "blog_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["blog_id"], name: "index_comments_on_blog_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
