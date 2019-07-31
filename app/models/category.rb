de8037a7cda26bf0385ca24a8a577fef90a6814d
yukimura1227 <takamura1227@gmail.com>
2019-07-31 15:38:53 +0900

    feat: category has_many events

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 # for Category
 class Category < ApplicationRecord
   belongs_to :chronology
+  has_many :events, -> { order(:year, :month) }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'association' do
-    describe 'belongs_to' do
+    describe 'belongs_to chronology' do
       it { is_expected.to belong_to(:chronology) }
+    end
+    describe 'has_many events' do
+      it { is_expected.to have_many(:events) }
     end
   end
 end
