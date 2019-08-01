3c567eed07a1eb9422717fc1f67a9c81969c91ab
yukimura1227 <takamura1227@gmail.com>
2019-08-01 17:34:03 +0900

    chore: add association to event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    describe 'belongs_to chronology' do
+      it { is_expected.to belong_to(:chronology) }
+    end
+    describe 'belongs_to category' do
+      it { is_expected.to belong_to(:category) }
+    end
+  end
 end
