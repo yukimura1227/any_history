b6856db9a2a843b8925deee5e8520c328d96ab3d
yukimura1227 <takamura1227@gmail.com>
2019-08-03 15:52:12 +0900

    feat: add link to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -72,6 +72,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :chronology_id, :year, :month, :category_id)
+      params.require(:event).permit(:title, :chronology_id, :year, :month, :category_id, :url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_155153) do
+ActiveRecord::Schema.define(version: 2019_08_03_063605) do
 
   create_table "categories", force: :cascade do |t|
     t.string "theme", default: "", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_155153) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "url"
     t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["chronology_id"], name: "index_events_on_chronology_id"
   end
