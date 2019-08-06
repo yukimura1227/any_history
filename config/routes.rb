4ac2a8fe2fab8f9fafd49c6a045433b36d33c4bf
yukimura1227 <takamura1227@gmail.com>
2019-08-06 17:08:56 +0900

    feat: chronologies/cards skelton

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :chronologies do
     collection do
+      get 'cards', to: 'chronologies#cards', as: :cards
       get 'view/:token', to: 'chronologies#view', as: :view
     end
   end
