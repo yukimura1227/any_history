a79d10294a21c80e6c27ce2b896d93d93df62d7d
yukimura1227 <takamura1227@gmail.com>
2019-08-06 22:15:00 +0900

    feat: add link to new chronology form

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[chronologies/view.css]
+Rails.application.config.assets.precompile += %w[chronologies/view.css chronologies/cards.css]
 Rails.application.config.assets.precompile += %w[chronologies/view.js]
 Rails.application.config.assets.precompile += %w[chronologies/cards.js]
